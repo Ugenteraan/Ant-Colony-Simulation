f0f5c104839f38ea7daca5a59c717689f0c0e868
Ugenteraan Manogaran <m.ugenteraan15@gmail.com>
2025-02-16 19:46:45 +0800

    colony init complete

--- src/simulation/colony.rs
+++ src/simulation/colony.rs
@@ -5,3 +5,19 @@ pub struct Colony {
 	position: Vec2,
 	food_in_store: u32,
 }
+
+
+pub impl Colony {
+
+
+	fn new(x: f32, y: f32, default_food_in_store: u32) -> Self {
+
+		Colony {
+			position: Vec2::new(x, y),
+			food_in_store: default_food_in_store,
+		}
+
+	}
+
+	
+}
